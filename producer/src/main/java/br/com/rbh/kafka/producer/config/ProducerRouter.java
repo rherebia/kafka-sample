6a967b0ddf2894fe3bd7422ba7cef5cdaf626aa3
Renato Herebia <renato.herebia@ufms.br>
2020-11-13 13:50:28 -0400

    Removendo header accept da requisição get

--- producer/src/main/java/br/com/rbh/kafka/producer/config/ProducerRouter.java
+++ producer/src/main/java/br/com/rbh/kafka/producer/config/ProducerRouter.java
@@ -1,15 +1,14 @@
 package br.com.rbh.kafka.producer.config;
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import br.com.rbh.kafka.producer.handler.MessageHandler;
-
-import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
 @Configuration
 public class ProducerRouter {
@@ -17,7 +16,6 @@ public class ProducerRouter {
 	@Bean
 	public RouterFunction<ServerResponse> productsRoute(MessageHandler messageHandler){
 		return RouterFunctions
-				.route(GET("/kafka").and(accept(MediaType.APPLICATION_JSON)), 
-						messageHandler::sendMessageToKafkaTopic);
+				.route(GET("/kafka"), messageHandler::sendMessageToKafkaTopic);
 	}
 }
